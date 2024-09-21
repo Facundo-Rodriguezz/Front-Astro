55e919ba10805614b7c2ae1197de2a5e52e05a1e
Facundo Rodriguez <fakundor05@gmail.com>
2024-09-20 22:52:34 -0300

    Astro estudio terminado

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,15 @@ import { defineConfig } from 'astro/config';
 
 import netlify from '@astrojs/netlify';
 
+import preact from '@astrojs/preact';
+
 // https://astro.build/config
 export default defineConfig({
+  site: "https://example.com",
   output: 'static',
+
   adapter: netlify({
   }),
+
+  integrations: [preact()],
 });
